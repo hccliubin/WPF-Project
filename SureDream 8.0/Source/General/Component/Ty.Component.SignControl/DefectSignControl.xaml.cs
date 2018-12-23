db86dcd541dad0c8a8ba08c9c243f81f2482472d
HeBianGu <908293466@qq.com>
2018-12-23 23:20:22 +0800

    11111

---
+++ SureDream 8.0/Source/General/Component/Ty.Component.SignControl/DefectSignControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ty.Component.SignControl
+{
+    /// <summary>
+    /// DefectSignControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class DefectSignControl : UserControl
+    {
+        public DefectSignControl()
+        {
+            InitializeComponent();
+
+            this.Loaded += DefectSignControl_Loaded;
+        }
+
+        private void DefectSignControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<TestModel> list = new ObservableCollection<TestModel>();
+
+            list.Add(new TestModel() {ID="0001",Name= "A001" } );
+            list.Add(new TestModel() { ID = "0002", Name = "A002" });
+            list.Add(new TestModel() { ID = "0003", Name = "A003" });
+            list.Add(new TestModel() { ID = "0004", Name = "A004" });
+            list.Add(new TestModel() { ID = "0005", Name = "A005" });
+            list.Add(new TestModel() { ID = "0006", Name = "A006" });
+            list.Add(new TestModel() { ID = "0007", Name = "A007" });
+            list.Add(new TestModel() { ID = "0008", Name = "A008" });
+
+            this.com.MyItemsSource = list;  
+        } 
+    }
+
+    public class TestModel
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ SureDream 8.0/Source/General/Component/Ty.Component.SignControl/ISign.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ty.Component.SignControl
+{
+    delegate void DefectSaveHandle(object sender, StrDefect defect);
+    delegate void SignSaveHandle(object sender, StrSign sign);
+
+
+    /// <summary>
+    /// 缺陷、样本标记控件接口
+    /// </summary>
+    interface ISign
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <returns>1:成功；其他失败(请注明失败时各代码含义)</returns>
+        int Initialize();
+        /// <summary>
+        /// 重置数据源
+        /// </summary>
+        /// <returns>1:成功；其他失败(请注明失败时各代码含义)</returns>
+        int DataSourceReset();
+        /// <summary>
+        /// 弹出样本标定框
+        /// </summary>
+        /// <param name="isDialog">是否对话框</param>
+        void ShowCalibration(bool isDialog);
+        /// <summary>
+        /// 弹出缺陷标记框
+        /// </summary>
+        /// <param name="isDialog">是否对话框</param>
+        void ShowDefect(bool isDialog);
+
+        /// <summary>
+        /// 设置焦点
+        /// </summary>
+        /// <param name="tabIndex">焦点控件index</param>
+        /// <returns></returns>
+        int Setfocus(int tabIndex);
+
+        /// <summary>
+        /// 设置控件数据源(Txt,lbl)
+        /// </summary>
+        /// <param name="tabIndex">index</param>
+        /// <param name="dataSource">数据源</param>
+        /// <returns></returns>
+        int SetDataSource(int tabIndex, object dataSource);
+        /// <summary>
+        /// 设置左侧描述信息
+        /// </summary>
+        /// <param name="tabIndex"></param>
+        /// <param name="describle"></param>
+        /// <returns></returns>
+        int SetDescribe(int tabIndex, string describle);
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <param name="tabIndex">index</param>
+        /// <param name="value">值</param>
+        /// <returns></returns>
+        int SetValue(int tabIndex, string value);
+
+        /// <summary>
+        /// 控件宽度
+        /// </summary>
+        int Width { get; set; }
+        /// <summary>
+        /// 控件高度
+        /// </summary>
+        int Height { get; set; }
+
+        //缺陷保存事件
+        event DefectSaveHandle DefectSave;
+        //样本保存事件
+        event SignSaveHandle SignSave;
+
+    }
+
+
+    /// <summary>
+    /// 缺陷 以下数据类型为暂定类型，后期需要修改
+    /// </summary>
+    struct StrDefect
+    {
+        //数据库类型
+        string DbType;
+        //数据采集方式
+        string AcquisitionMethod;
+        //铁路局
+        string RailwayAdministration;
+        //线路名
+        string LineName;
+        //站区
+        string Station;
+        //基本单元
+        string BackUnit;
+        //设备设施种类
+        string DevType;
+        //设备参数项目
+        string DevParmPrj;
+        //设备参数属性
+        string DevParmPrp;
+        //设备参数参考
+        string DevRefVal;
+        //测试日期yyyy-MM-dd
+        string TestDate;
+        //公里标K000+000.0
+        string KmScale;
+        //创建人
+        string Creater;
+        //检测车
+        string TestTrain;
+        //责任车间
+        string ResponseShop;
+        //责任工区
+        string ResponseArea;
+
+    }
+    /// <summary>
+    /// 样本标定 以下数据类型为暂定类型，后期需要修改
+    /// </summary>
+    struct StrSign
+    {
+        //区域/部位
+        string Part;
+        //装置类型
+        string DevType;
+        //创建人
+        string Creater;
+    }
+    /// <summary>
+    /// 控件类型
+    /// </summary>
+    enum CType
+    {
+        //文本输入
+        Txt,
+        //下拉列表
+        Cbb,
+        //文本
+        Lbl
+    }
+
+}
